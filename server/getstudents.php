e394dd1ab7a9472e2daf84a5494a2670108fff9b
Quy Truong <qmtruong2@gmail.com>
2019-02-15 14:14:11 -0800

    conected frontend and backend

--- server/getstudents.php
+++ server/getstudents.php
@@ -1,5 +1,24 @@
 <?php
 
+require_once('mysqlcredentials.php');
+
+$query = "SELECT * FROM `students`";
 
 
+$result = mysqli_query($db, $query);
+
+$output = [
+    'success' =>false
+];
+$data = [];
+
+while($row = mysqli_fetch_assoc($result)) {
+      array_push($data, $row);
+}
+$output['data'] = $data;
+$output['success'] = true;
+
+$json_output = json_encode( $output);
+
+print( $json_output);
 ?>
